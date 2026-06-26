31bac7b33e15b18ee2fcd9df90dc4a5e9f73e3b3
akash1810 <akash1810@users.noreply.github.com>
2026-06-26 19:27:16 +0100

    fix: Reset content of gh-pages branch

--- fun-background/values.js
+++
@@ -1,1 +0,0 @@
-export const GitHubValues = { "githubCommitSha": "17e911994f2564aa4a8bbcac32a67b275395e38a", "userCommitSha": "fdeb40fd34f26f331063e99c225d0694f672a373" }
--- page-footer/values.js
+++
@@ -1,1 +0,0 @@
-export const GitHubValues = { "githubCommitSha": "8a00613fdb331184d6cc2fc69a4417daa49c0913", "userCommitSha": "5112708d55ea7cc99af8a19e24b6d21f0ec42c58" }
